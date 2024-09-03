f38faa41499ba99c2a2cba6c95de1541b3b3ce9f
Oulmane <e2073117@cmaisonneuve.qc.ca>
2024-09-03 11:01:48 -0400

    Externalization

--- parties-communes/entete.php
+++ parties-communes/entete.php
@@ -1,3 +1,22 @@
+<?php
+	// A) Lire le fichier JSON contenant les textes
+	// Etape 1 :"lire" le fichier "i18n/fr.json"
+	// et placer son contenu a une variable PHP
+	$textesJSON = file_get_contents("i18n/fr.json");
+
+	// Etape 2 : Converir le contenu JSON du fichier en variable PHP
+	// pour remettre les textes dans la page web aux bons endroits
+	$textes = json_decode($textesJSON);
+
+	$_ent = $textes->entete;
+	$pp = $textes->pp;
+
+	// print_r($textesConverties);
+	// Imprimer la propriete alt-logo de l'objet correspondant a la propriete entete de cet objet
+	// echo $textesConverties->entete->altLogo;
+	// echo $textesConverties->entete->placeholderRecherche;
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -24,18 +43,18 @@
 			</nav>
 			<nav class="barre-logo">
 				<label for="cc-btn-responsive" class="material-icons burger">menu</label>
-				<a class="logo" href="index.php"><img src="images/logo.png" alt="Accueil"></a>
+				<a class="logo" href="index.php"><img src="images/logo.png" alt="<?php echo $_ent->altLogo;?>"></a>
 				<a class="material-icons panier" href="panier.php">shopping_cart</a>
-				<input class="recherche" type="search" name="motscles" placeholder="Recherche">
+				<input class="recherche" type="search" name="motscles" placeholder="<?php echo $_ent->placeholderRecherche;?>">
 			</nav>
 			<input type="checkbox" id="cc-btn-responsive">
 			<nav class="principale">
 				<label for="cc-btn-responsive" class="menu-controle material-icons">close</label>
-				<a href="teeshirts.php" class="<?php if ($page =="teeshirts") {echo("actif");}?> ">Teeshirts</a>
-				<a href="casquettes.php">Casquettes</a>
-				<a href="hoodies.php">Hoodies</a>
+				<a href="teeshirts.php" class="<?php if ($page =="teeshirts") {echo("actif");}?> "><?= $_ent->menuTeeshirts;?></a>
+				<a href="casquettes.php"><?=$_ent->menuCasquettes?></a>
+				<a href="hoodies.php"><?=$_ent->menuHoodies?></a>
 				<span class="separateur"></span>
-				<a href="aide.php">Aide</a>
-				<a href="apropos.php">À propos de nous</a>
+				<a href="aide.php"><?=$_ent->menuAide?></a>
+				<a href="apropos.php"><?=$_ent->menuNous?></a>
 			</nav>
 		</header>
